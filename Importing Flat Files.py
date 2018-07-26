9e8e47ed1178cff6833e8407eb81ba1f79591b0c
fernando.apolinar <fernandoapolinar@gmail.com>
2018-07-26 15:27:58 -0500

    Random Walks

--- Importing Flat Files.py
+++ Importing Flat Files.py
@@ -13,6 +13,11 @@ print(type(digits))
 
 print(digits.shape)
 
+#Reading with different separator
+
+#data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=(0,2)) #Skips first row adn tab as separator, uses just column 1 and 3
+
+
 
 
 
