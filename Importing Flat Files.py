a76400d8f6a538e12429b3ee48c40764f2be9ba3
fernando.apolinar <fernandoapolinar@gmail.com>
2018-07-27 16:39:11 -0500

    Flat files, multiples types

--- Importing Flat Files.py
+++ Importing Flat Files.py
@@ -17,8 +17,29 @@ print(digits.shape)
 
 #data = np.loadtxt(file, delimiter='\t', skiprows=1, usecols=(0,2)) #Skips first row adn tab as separator, uses just column 1 and 3
 
+#Import data as floats and skip the first row: data_float dtype=str
+#data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
 
 
+#-------------------------------------#
+#Data set with multiple data types
+#np.genfromtxt is an structured array handling umutiple data types.
 
 
+#data = np.genfromtxt('titanic.csv', delimiter=',', names=True, dtype=None)
+#print(data.dtype.names)
+#data[i] call to rows
+#data['Column'] call to  columns
+
+
+#------------------------------------#
+#np.recfromcsv has none type and , as default separator
+# Assign the filename: file
+#file = 'titanic.csv'
+
+# Import file using np.recfromcsv: d
+#d = np.recfromcsv(file)
+
+# Print out first three entries of d
+#print(d[:3])
 
